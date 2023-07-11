ce1d60ee885b05a682c0d53060e2185760325f0d
黄鑫 <990695619@qq.com>
2023-07-11 10:37:58 +0800

    引入svg

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,26 @@
 <template>
     <layout-wrapper>
+        <svg>
+            <use xlink:href="#labels" />
+        </svg>
+        <svg>
+            <use xlink:href="#money" />
+        </svg>
+        <svg>
+            <use xlink:href="#statistics" />
+        </svg>
         Labels Page
     </layout-wrapper>
 </template>
 
 <script lang="ts">
+    import x from '@/assets/icons/labels.svg';
+    import y from '@/assets/icons/money.svg';
+    import z from '@/assets/icons/statistics.svg';
+    console.log(x)
+    console.log(y)
+    console.log(z)
+    // /img/labels.c5b46c51.svg
     export default {
     }
 </script>
